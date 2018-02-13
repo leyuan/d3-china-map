2b7aa900e73d197014f439e7c80d6094308c7514
Leyuan <leyuan.ly@gmail.com>
2018-02-13 13:44:28 -0700

    restructure

---
+++ server.js
@@ -0,0 +1,24 @@
+// call the packages we need
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;        // set our port
+
+// ROUTES FOR OUR API
+var router = express.Router();
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', router);
+
+app.get('/', function(req, res) {
+    res.sendfile('public/index.html');
+});
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
